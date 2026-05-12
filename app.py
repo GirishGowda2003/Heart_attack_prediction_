e387d84e3386f6fa62bb84254498c3816a43fcd4
Girish M <girishgowda62003@gmail.com>
2026-05-12 11:00:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import pickle
 import streamlit as st
 import numpy as np
 import pandas as pd
-import seaborn as sns
+# import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 
